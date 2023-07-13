850a10494597493680e74957351eb2c31adff49b
Nandini soni <nandini.soni@nagarro.com>
2023-07-13 22:13:51 +0530

    first commit

---
+++ src/main/java/com/example/project/Employee.java
@@ -0,0 +1,57 @@
+package com.example.project;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name = "Employee")
+	private String name;
+	
+	@Column
+	private float salary;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String name, float salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
+}
